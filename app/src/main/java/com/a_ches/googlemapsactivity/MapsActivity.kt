7631ba363b4cc11d6ca7fb9990196c879d84fca1
APopova <a_ches@mail.ru>
2022-05-10 23:35:35 +0300

    Init commit

---
+++ app/src/main/java/com/a_ches/googlemapsactivity/MapsActivity.kt
@@ -0,0 +1,18 @@
+package com.a_ches.googlemapsactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.a_ches.googlemapsactivity.databinding.ActivityMapsBinding
+
+
+class MapsActivity  {
+
+
+}
